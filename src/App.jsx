d0c66e4a3fd8abe6cc80a00cfd1a1facae242956
Merk48 <meryKassis48@gmail.com>
2025-07-19 17:49:33 +0300

    feat: add app pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-function App() {
-  const x = 3;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
 
-  return <div>WorldWise</div>;
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+// Declarative
