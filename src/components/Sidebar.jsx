e0242ee36d8662eecb349d3cf16cec009cdc7963
Merk48 <meryKassis48@gmail.com>
2025-07-20 15:36:13 +0300

    feat : cities & countries tab component and routing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import AppSidebarFooter from "./AppSidebarFooter";
 import Logo from "./Logo";
@@ -7,7 +8,7 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of citites</p>
+      <Outlet />
       <AppSidebarFooter />
     </div>
   );
