c2e1f8b051c19d79d8317a9bdd474cb0f48d51a7
Merk48 <meryKassis48@gmail.com>
2025-07-23 03:04:59 +0300

    fix: map bug

--- src/contexts/authentication/fakeAuthenticationContext.jsx
+++ src/contexts/authentication/fakeAuthenticationContext.jsx
@@ -2,8 +2,8 @@
 import { createContext, useContext, useReducer } from "react";
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "Mery",
+  email: "mery@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
